ee1431b6e6bb56d7de917bd441a3279c6f4924ac
小白萌新 <3036403996@qq.com>
2024-04-14 00:28:14 +0800

    feat: 根据屏幕宽度来判断是否是移动设备

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import { isMobileTerminal } from '@/utils/flexible'
 </script>
 
 <template>
-  <div class="bg-red-900 font-bold">app</div>
+  <div class="bg-red-900 font-bold">{{ isMobileTerminal }}</div>
 </template>
 
 <style scoped>
