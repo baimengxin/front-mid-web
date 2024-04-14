d5e404e1a5e96045ef2cde25fc4f5b8f2bbd6a7c
小白萌新 <3036403996@qq.com>
2024-04-15 03:31:34 +0800

    refactor: vite 通用组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,21 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+/**
+ * vite 自动注册组件
+ * 1. 使用 vite 的 Glob 导入 功能：该功能可以帮助我们在 文件系统中导入多个模块 import.meta.glob()
+ * 2. vue 的 defineAsyncComponent 方法，该方法可以创建一个 按需加载的异步组件
+ * */
+import { defineAsyncComponent } from 'vue'
 
 export default {
   install(app) {
-    app.component('m-svg-icon', svgIcon)
-    app.component('m-popup', popup)
+    // 1. 获取当前路径任意文件夹下的 index.vue 文件
+    // ('./*/index.vue') 当前文件下的，任意文件下的，index.vue 文件
+    const components = import.meta.glob('./*/index.vue')
+    // 2. 遍历获取到的组件模块
+    for (const [fullPath, fn] of Object.entries(components)) {
+      // 拼接组件注册的 name
+      const componentName = 'm-' + fullPath.replace('./', '').split('/')[0]
+      // 3. 通过 defineAsyncComponent 异步导入指定路径下的组件
+      app.component(componentName, defineAsyncComponent(fn))
+    }
   }
 }
