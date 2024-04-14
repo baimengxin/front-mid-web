8efdd1c0494aa6238f7a962bd486ce6ffdf9749a
小白萌新 <3036403996@qq.com>
2024-04-14 23:32:59 +0800

    feat: 通用组件-svgIcon 组件

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import App from './App.vue'
 import router from '@/router'
 import { useREM } from '@/utils/flexible'
 
+// 通用组件
+import mLibs from '@/libs'
+
 // 根据屏幕宽度初始化字体大小
 useREM()
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(mLibs).mount('#app')
