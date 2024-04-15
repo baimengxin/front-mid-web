5549622c6c90368c0a65ba75eb3032f86c628e74
小白萌新 <3036403996@qq.com>
2024-04-15 23:45:39 +0800

    feat: PC 端 Header 模块处理分析与简单实现

--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -1,7 +1,28 @@
-<script setup></script>
+<script setup>
+import headerSearchVue from './header-search/index.vue'
+import headerThemeVue from './header-theme.vue'
+import headerMyVue from './header-my.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const onToHome = () => {
+  router.push('/')
+}
+</script>
 
 <template>
-  <div class="">header</div>
+  <div class="w-full bg-white border-b border-b-zinc-200 px-2 py-1">
+    <div class="flex items-center">
+      <img
+        class="h-4 cursor-pointer mr-2"
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+        @click="onToHome"
+      />
+      <headerSearchVue class="mr-1" />
+      <headerThemeVue class="mr-1" />
+      <headerMyVue />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
