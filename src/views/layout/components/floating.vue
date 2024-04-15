26dfd6e05ab86aa77f95d1e77e5d47d35ff4a9c9
小白萌新 <3036403996@qq.com>
2024-04-15 13:58:26 +0800

    feat: PC 端基础架构

---
+++ src/views/layout/components/floating.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="fixed right-2 bottom-10">floating</div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,7 +1,15 @@
-<script setup></script>
+<script setup>
+import headerVue from './components/header/index.vue'
+import mainVue from './components/main.vue'
+import floatingVue from './components/floating.vue'
+</script>
 
 <template>
-  <div class="">layout</div>
+  <div class="h-screen">
+    <headerVue class="h-header" />
+    <mainVue class="h-main" />
+    <floatingVue />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
