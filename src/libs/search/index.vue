d8de6363a922e1d8639e26e643154e815b834195
小白萌新 <3036403996@qq.com>
2024-04-16 22:33:06 +0800

    feat: 通用组件-search 组件完善基本功能

--- src/libs/search/index.vue
+++ src/libs/search/index.vue
@@ -1,8 +1,92 @@
-<script setup></script>
+<script>
+// 更新事件
+const EMIT_UPDATE_MODELVALUE = 'update:modelValue'
+// 触发搜索（点击或回车）事件
+const EMIT_SEARCH = 'search'
+// 删除所有文本事件
+const EMIT_CLEAR = 'clear'
+// 输入事件
+const EMIT_INPUT = 'input'
+// 获取焦点事件
+const EMIT_FOCUS = 'focus'
+// 失去焦点事件
+const EMIT_BLUR = 'blur'
+</script>
+
+<script setup>
+import { onClickOutside, useVModel } from '@vueuse/core'
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emits = defineEmits([
+  EMIT_UPDATE_MODELVALUE,
+  EMIT_SEARCH,
+  EMIT_CLEAR,
+  EMIT_INPUT,
+  EMIT_FOCUS,
+  EMIT_BLUR
+])
+
+// 双向绑定
+const inputValue = useVModel(props)
+/**
+ * 监听用户输入事件
+ * */
+watch(inputValue, (val) => {
+  emits(EMIT_INPUT, val)
+})
+
+/**
+ * 一键清空文本
+ * */
+const onClearClick = () => {
+  inputValue.value = ''
+  emits(EMIT_CLEAR, '')
+}
+
+/**
+ * 搜索事件
+ */
+const onSearchHandlder = () => {
+  emits(EMIT_SEARCH, inputValue.value)
+}
+
+// input 是否获取到焦点
+const isFocus = ref(false)
+/**
+ * 获取到焦点
+ */
+const onFocusHandler = () => {
+  isFocus.value = true
+  emits(EMIT_FOCUS)
+}
+
+const containerTarget = ref(null)
+/**
+ * 点击区域外，隐藏 dropdown
+ */
+onClickOutside(containerTarget, () => {
+  isFocus.value = false
+})
+
+/**
+ * 失去焦点
+ */
+const onBlurHandler = () => {
+  emits(EMIT_BLUR)
+}
+</script>
 
 <template>
   <div
     class="group relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40"
+    ref="containerTarget"
   >
     <div>
       <!-- 搜索图标 -->
@@ -18,24 +102,36 @@
         class="block w-full h-[44px] pl-4 text-sm outline-0 bg-zinc-100 caret-zinc-400 rounded-xl text-zinc-900 tracking-wide font-semibold border border-zinc-100 duration-500 group-hover:bg-white group-hover:border-zinc-200 focus:border-red-300"
         type="text"
         placeholder="搜索"
+        v-model="inputValue"
+        @keyup.enter="onSearchHandlder"
+        @focus="onFocusHandler"
+        @blur="onBlurHandler"
       />
 
       <!-- 删除按钮 -->
       <m-svg-icon
         name="input-delete"
         class="h-1.5 w-1.5 absolute translate-y-[-50%] top-[50%] right-9 duration-500 cursor-pointer"
+        v-show="inputValue"
+        @click="onClearClick"
       ></m-svg-icon>
-
+      <!-- 分割线 -->
+      <div
+        class="opacity-0 h-1.5 w-[1px] absolute translate-y-[-50%] top-[50%] right-[62px] duration-500 bg-zinc-200 group-hover:opacity-100"
+      ></div>
       <!-- TODO: 搜索按钮(通用组件) -->
       <m-button
-        class="absolute translate-y-[-50%] top-[50%] right-1 rounded-full"
+        class="absolute translate-y-[-50%] top-[50%] right-1 rounded-xl duration-500 opacity-0 group-hover:opacity-100"
         iconColor="#ffffff"
         icon="search"
+        @click="onSearchHandlder"
       ></m-button>
     </div>
 
     <transition name="slide">
       <div
+        v-if="$slots.dropdown"
+        v-show="isFocus"
         class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-3xl"
       >
         <slot name="dropdown" />
--- src/views/layout/components/header/header-search/index.vue
+++ src/views/layout/components/header/header-search/index.vue
@@ -1,8 +1,15 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const inputValue = ref('')
+</script>
 
 <template>
   <div class="w-full">
-    <m-search></m-search>
+    <m-search v-model="inputValue">
+      <template #dropdown>
+        <div>123</div>
+      </template>
+    </m-search>
   </div>
 </template>
 
