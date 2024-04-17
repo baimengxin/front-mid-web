f1301e4be8f787dd98dc34dbad93b0980f0bd540
小白萌新 <3036403996@qq.com>
2024-04-17 22:41:34 +0800

    fix: 解决 navigationBar 闪烁问题，添加初始化数据，使用持久化方案

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import { createPinia } from 'pinia'
+// 1. 引用
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 export * from './modules/category'
 
 const pinia = createPinia()
+
+// 2. 添加到 pinia 实例上
+pinia.use(piniaPluginPersistedstate)
 
 export default pinia
--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -1,17 +1,24 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { getCategory } from '@/api/category'
-import { ALL_CATEGORY_ITEM } from '@/constants'
+import { ALL_CATEGORY_ITEM, CATEGORY_NOMAR_DATA } from '@/constants'
 
-export const useCategoryStore = defineStore('category', () => {
-  const categoryData = ref([ALL_CATEGORY_ITEM])
+export const useCategoryStore = defineStore(
+  'category',
+  () => {
+    const categoryData = ref(CATEGORY_NOMAR_DATA)
 
-  /**
-   * 获取分类列表数据
-   * */
-  const loadCategoryFn = async () => {
-    const { categorys } = await getCategory()
-    categoryData.value = [ALL_CATEGORY_ITEM, ...categorys]
+    /**
+     * 获取分类列表数据
+     * */
+    const loadCategoryFn = async () => {
+      const { categorys } = await getCategory()
+      categoryData.value = [ALL_CATEGORY_ITEM, ...categorys]
+    }
+    return { categoryData, loadCategoryFn }
+  },
+  {
+    // 3. 开启默认持久化配置
+    persist: true
   }
-  return { categoryData, loadCategoryFn }
-})
+)
