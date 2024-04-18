f6e40cf68008fc185f66dac6b73e1ba4512d9cb9
小白萌新 <3036403996@qq.com>
2024-04-18 22:15:59 +0800

    fix: 解决列表未居中的问题

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -6,7 +6,7 @@ import ListVue from './components/list/index.vue'
 <template>
   <div class="h-full overflow-auto bg-white dark:bg-zinc-800 duration-500">
     <Navigation />
-    <div class="max-w-scrren-xl mx-auto relative m-1 xl:mt-4">
+    <div class="max-w-screen-xl mx-auto relative m-1 xl:mt-4">
       <ListVue />
     </div>
   </div>
