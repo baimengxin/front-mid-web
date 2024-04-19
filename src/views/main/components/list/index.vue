3f71a63e0977fbf7a59ecaf4672aa142c1ae8102
小白萌新 <3036403996@qq.com>
2024-04-20 01:35:16 +0800

    feat: 应用 长列表组件 和 瀑布流组件实现 长列表瀑布流效果

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -9,26 +9,59 @@ let query = {
   size: 20
 }
 
+// 数据是否在加载中
+const isLoading = ref(false)
+// 数据是否全部加载完成
+const isFinished = ref(false)
+// 数据源
 const pexelsList = ref([])
 const getPexelsData = async () => {
+  // 数据全部加载完成 则 return
+  if (isFinished.value) return
+
+  // 完成第一次请求之后，后续让 page 自增
+  if (pexelsList.value.length) {
+    query.page += 1
+  }
+
+  // 触发接口请求
   const res = await getPexelsList(query)
-  pexelsList.value = res.list
+
+  // 初始请求清空数据源
+  if (query.page === 1) {
+    pexelsList.value = res.list
+  } else {
+    pexelsList.value.push(...res.list)
+  }
+
+  // 判断数据是否全部加载完成
+  if (pexelsList.value.length === res.total) {
+    isFinished.value = true
+  }
+
+  // 修改 loading 标记
+  isLoading.value = false
 }
-getPexelsData()
 </script>
 
 <template>
-  <m-waterfall
-    class="w-full px-1"
-    :data="pexelsList"
-    nodeKey="id"
-    :column="isMobileTerminal ? 2 : 5"
-    :picturePreReading="true"
+  <m-infinite
+    v-model="isLoading"
+    :isFinished="isFinished"
+    @onLoad="getPexelsData"
   >
-    <template v-slot="{ item, width }">
-      <ItemVue :data="item" />
-    </template>
-  </m-waterfall>
+    <m-waterfall
+      class="w-full px-1"
+      :data="pexelsList"
+      nodeKey="id"
+      :column="isMobileTerminal ? 2 : 5"
+      :picturePreReading="false"
+    >
+      <template v-slot="{ item, width }">
+        <ItemVue :data="item" :width="width" />
+      </template>
+    </m-waterfall>
+  </m-infinite>
 </template>
 
 <style lang="scss" scoped></style>
