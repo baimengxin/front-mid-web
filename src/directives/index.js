67799b6cba591965dd075187df6a1a1348b45fa7
小白萌新 <3036403996@qq.com>
2024-04-20 03:57:58 +0800

    feat: vite 的自动注册自定义指令

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,10 +1,18 @@
-import lazy from './module/lazy'
-
 /**
  * 全局注册指令
  */
 export default {
-  install(app) {
-    app.directive('lazy', lazy)
+  async install(app) {
+    // 获取 module 文件下的任意 js 文件
+    const directives = import.meta.glob('./module/*.js')
+
+    for (const [key, value] of Object.entries(directives)) {
+      const d = await value()
+      // 拼接
+      const arr = key.split('/')
+      const name = arr[arr.length - 1].replace('.js', '')
+      // 注册指令
+      app.directive(name, d.default)
+    }
   }
 }
