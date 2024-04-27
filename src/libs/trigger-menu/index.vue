4f8715cc7767e04a5acdc7a5fb8498a05b617bac
小白萌新 <3036403996@qq.com>
2024-04-28 03:51:52 +0800

    feat: 通用组件-trigger-menu 和 trigger-menu-item 构建

---
+++ src/libs/trigger-menu/index.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div
+    class="max-w-[180px] bg-white dark:bg-zinc-800 rounded-full shadow flex justify-between items-center px-2 py-1"
+  >
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
