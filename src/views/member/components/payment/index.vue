4ae0a5ba8d1570593c33c336e680f264d8ef8993
小白萌新 <3036403996@qq.com>
2024-04-29 15:37:31 +0800

    feat: VIP页面-移动端支付布局样式处理

--- src/views/member/components/payment/index.vue
+++ src/views/member/components/payment/index.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible'
 import PcPaymentVue from './pc-payment/index.vue'
+import MobilePaymentVue from './mobile-payment/index.vue'
 </script>
 
 <template>
   <div>
     <!-- pc 端支付 -->
     <PcPaymentVue v-if="!isMobileTerminal" />
+
+    <!-- 移动端支付 -->
+    <MobilePaymentVue v-else />
   </div>
 </template>
 
